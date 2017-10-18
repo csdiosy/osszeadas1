22783461dc354c87137b8b9fa8f5307f83d9f7bd
csdiosy <csdiosy@gmx.net>
2017-10-18 17:13:33 +0200

    gomb hivja a beépített scriptes fuggvenyt

---
+++ osszeg.js
@@ -0,0 +1,11 @@
+function szamolj1() {
+aler("fuggvenyben vagyok");
+};
+
+function szamolj() {
+    var a = document.getElementById("szam1").value;
+    var b = document.getElementById("szam2").value;
+    var c = a+b;
+    document.getElementById("eredmeny").value=c;
+    //er.innerHTML(c);
+    };
