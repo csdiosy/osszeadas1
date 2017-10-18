ced254b63bde50d5cad111f91a88194424566bfe
csdiosy <csdiosy@gmx.net>
2017-10-18 17:21:16 +0200

    Szamokká alakítás

--- osszeg.js
+++ osszeg.js
@@ -3,9 +3,8 @@ alert("fuggvenyben vagyok");
 };
 
 function szamolj() {
-    var a = document.getElementById("szam1").value;
-    var b = document.getElementById("szam2").value;
+    var a = Number(document.getElementById("szam1").value);
+    var b = Number(document.getElementById("szam2").value);
     var c = a+b;
     document.getElementById("eredmeny").value=c;
-    //er.innerHTML(c);
     };
